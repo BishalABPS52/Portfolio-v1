7e7a26a71e204bcfc952a81b593514dd9a0974ce
Bishal Shrestha <167290532+BishalABPS52@users.noreply.github.com>
2025-04-04 07:17:27 +0545

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,6 @@ export default function Home() {
     return (
       <section className="flex flex-col justify-center items-center min-h-screen bg-gray-900 text-white">
         <h1 className="text-5xl font-bold mb-6">Bishal Shrestha</h1>
-        <p className="text-xl mb-6">Bachelor in Computer Engineering at IOE, Thapathali Campus</p>
-        <p className="text-lg mb-6">
-          I am a passionate web developer, freelancer, and technologist working on exciting projects.
-        </p>
         <div>
           <p className="text-lg mb-3">Check out my work:</p>
           <ul className="flex space-x-6">
